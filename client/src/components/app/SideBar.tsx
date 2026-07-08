097fed334ffc76af7f1e8158b268cc8172fcab76
midnite-ops <charlesoyims@gmail.com>
2026-07-08 17:15:35 +0100

    Created the header and the sidebar components for the app

--- client/src/components/app/SideBar.tsx
+++ client/src/components/app/SideBar.tsx
@@ -3,6 +3,20 @@ import { Link } from "react-router"
 
 const SideBar = () => {
     const [activeLink, setActiveLink] = useState('For You')
+    const creators = [
+        {
+            name: 'Jesse Oyims',
+            userName: '@midniteOps'
+        },
+        {
+            name: 'Jesse Oyims',
+            userName: '@midniteOps'
+        },
+        {
+            name: 'Jesse Oyims',
+            userName: '@midniteOps'
+        }
+    ]
     const sidebarLinks = [
         {
             title: 'For You',
@@ -18,12 +32,35 @@ const SideBar = () => {
         }
     ]
   return (
-    <section className='fixed left-0 top-0 bottom-0 border-r border-subtitleText/25'>
-        <ul>
+    <section className='fixed left-0 flex flex-col top-0 bottom-0 border-r border-subtitleText/25 w-50 pb-10  px-5 pt-30'>
+        <ul className="flex flex-col gap-2 font-semibold border-b border-subtitleText/25 pb-10 mb-5">
             {sidebarLinks.map((item) => (
                <Link to={item.link} className={`${activeLink === item.title ? 'text-white' : 'text-subtitleText'} cursor-pointer`} onClick={() => setActiveLink(item.title)}>{item.title}</Link>
             ))}
         </ul>
+
+        <div>
+            <h1 className="text-lg">Top Creators</h1>
+            <ul className="flex gap-5 flex-col mt-5">
+                {creators.map((item) => (
+                    <li className="flex gap-2 cursor-pointer">
+                        <div className="bg-actionsColor size-8 rounded-full"></div>
+                        <div>
+                            <h4 className="text-sm font-semibold">{item.name}</h4>
+                            <p className="text-xs text-subtitleText">{item.userName}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+        <div className="flex-1 items-end flex">
+            <div className="flex gap-2 items-center border-t border-subtitleText/25 w-full pt-5">
+                <div className="bg-actionsColor size-8 rounded-full"></div>
+                <h4 className="text-sm font-semibold cursor-pointer">Account</h4>
+            </div>
+            
+        </div>
     </section>
   )
 }
--- client/src/pages/app/appHome.tsx
+++ client/src/pages/app/appHome.tsx
@@ -1,10 +1,11 @@
+import Header from "../../components/app/Header"
 import SideBar from "../../components/app/SideBar"
 
 const AppFeed = () => {
   return (
     <main className="bg-bgBlack text-white h-screen">
+        <Header />
         <SideBar />
-        hello
     </main>
   )
 }
