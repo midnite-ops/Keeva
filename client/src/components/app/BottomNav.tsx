83dd46ed22fc4ca552444b17dcb54c6158cea8f7
midnite-ops <charlesoyims@gmail.com>
2026-07-10 22:15:20 +0100

    Fixed the bottom nav color

--- client/src/components/app/BottomNav.tsx
+++ client/src/components/app/BottomNav.tsx
@@ -6,31 +6,31 @@ const BottomNav = () => {
     const [currentLink, setCurrentLink] = useState('app/feed')
     const navLinks = [
         {
-            icon: <Home />,
+            icon: Home ,
             link: 'app/feed'
         },
         {
-            icon: <SearchIcon/>,
+            icon: SearchIcon,
             link: 'app/explore'
         },
         
         {
-            icon: <Plus />,
+            icon: Plus,
             link: 'app/create-post'
         },
         {
-            icon: <Bell />,
+            icon: Bell ,
             link: 'app/notification'
         },
         {
-            icon: <User2 />,
+            icon: User2 ,
             link: 'app/account'
         }
     ]
   return (
     <nav className='fixed px-4 py-4 z-10 bottom-0 right-0 w-full h-15 md:hidden bg-background flex justify-between items-center'>
         {navLinks.map((item) => (
-            <Link to={item.link}  className={`${item.link === currentLink ? 'fill-white stroke-black' : ''}`}>{item.icon}</Link>
+            <Link to={item.link} onClick={() => setCurrentLink(item.link)}> <item.icon fill={currentLink === item.link ? 'white' : ''}/></Link>
         ))}
     </nav>
   )
