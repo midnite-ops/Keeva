58098ca7b161d23fa1b2fca7cfbcc802f2dfe725
midnite-ops <charlesoyims@gmail.com>
2026-07-11 15:34:38 +0100

    Created mock data to display the outfits, also created the types folder

---
+++ client/src/data/users.ts
@@ -0,0 +1,33 @@
+import type { Users } from '../types/types'
+
+const data: Users[] = [
+  {
+    id: 1,
+    name: "Jesse",
+    username: 'midniteOps',
+    profilePic: 'afdaf',
+    dateOfBirth: '11-22-2033',
+    location: 'dafsf',
+    role: "creator"
+  },
+  {
+    id: 2,
+    name: "Sarah",
+    username: 'midniteOps',
+    profilePic: 'afdaf',
+    dateOfBirth: '11-22-2033',
+    location: 'dafsf',
+    role: "vendor"
+  },
+  {
+    id: 3,
+    name: "David",
+    username: 'midniteOps',
+    profilePic: 'afdaf',
+    dateOfBirth: '11-22-2033',
+    location: 'dafsf',
+    role: "customer"
+  }
+]
+
+export default data
